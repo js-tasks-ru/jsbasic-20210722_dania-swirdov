c5bc6ff157853f024baa795c93d5f5c51978445b
SupaProgramer <dania.swirdov@gmail.com>
2021-07-28 14:17:44 +0300

    Решена задача 3 модуля 2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,8 @@
 let calculator = {
   // ваш код
+  read: (a, b) => {this.a = a; this.b = b;},
+  sum: (a, b) => this.a + this.b,
+  mul: (a, b) => this.a * this.b
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
