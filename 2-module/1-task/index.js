6c18da08313c516a63895a81a6985ee9191dc4c5
SupaProgramer <dania.swirdov@gmail.com>
2021-07-28 14:08:20 +0300

    Решена задача 1 модуля 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,8 @@
 function sumSalary(salaries) {
   // ваш код...
+  let res = 0;
+  for (value of Object.values(salaries)) {
+    if (typeof value == 'number' && isFinite(value) && !isNaN(value)) res += value; 
+  }
+  return res;
 }
