1a453043a674dd58d2e4647ff7343705baef3481
SupaProgramer <dania.swirdov@gmail.com>
2021-07-30 10:08:40 +0300

    Решена задача 3 модуля 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,8 @@
 function camelize(str) {
   // ваш код...
+  return str.split('').map((char, i, arr) => {
+    if(char == '-') return;
+    if(arr[i-1] == '-') return char.toUpperCase();
+    return char;
+  }).join('');
 }
