ecd3e6ba9d077f7b4501605d309d28a23932c4e4
SupaProgramer <dania.swirdov@gmail.com>
2021-08-02 14:52:39 +0300

    Решена задача 3 модуля 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,13 @@
 function highlight(table) {
   // ваш код...
+  for (row of table.querySelector('tbody').rows) {
+    let status = row.querySelector('[data-available]');
+    if (status) {
+      if (status.dataset.available == 'true') row.classList.add('available');
+      else row.classList.add('unavailable');
+    } else row.setAttribute('hidden', true);
+    if (row.cells[2].textContent == 'm') row.classList.add('male');
+    if (row.cells[2].textContent == 'f') row.classList.add('female');
+    if (row.cells[1].textContent < 18) row.style.textDecoration = 'line-through';
+  }
 }
