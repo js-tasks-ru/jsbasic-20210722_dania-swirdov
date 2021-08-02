234f7b2b1e58d6d539a41c3a4702d43427e56472
SupaProgramer <dania.swirdov@gmail.com>
2021-08-02 14:15:47 +0300

    Решена задача 1 модуля 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let res = document.createElement('ul');
+  for (friend of friends) {
+    let li = document.createElement('li');
+    li.textContent = `${friend['firstName']} ${friend['lastName']}`;
+    res.append(li);
+  }
+  return res;
 }
