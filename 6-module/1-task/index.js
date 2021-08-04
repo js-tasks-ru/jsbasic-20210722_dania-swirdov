a5954519f79e246fcd4ff456931c0d48ff079ba1
SupaProgramer <dania.swirdov@gmail.com>
2021-08-04 22:24:37 +0300

    Задача 1 Модуль 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,35 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = `    
+    <thead>
+      <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>`;
+    for (let user of rows) {
+      let row = document.createElement('tr');
+      let cross = document.createElement('td');
+      let crossBtn = document.createElement('button');
+      crossBtn.textContent = 'X';
+      cross.append(crossBtn);
+      for (let value of Object.values(user)) {
+        let cell = document.createElement('td');
+        cell.textContent = value;
+        row.append(cell);
+      }
+      row.append(cross);
+      this.elem.querySelector('tbody').append(row);
+    }
+    this.elem.addEventListener('click', (event) => {
+      if (event.target.textContent == 'X') event.target.closest('tr').remove();
+    });
   }
 }
